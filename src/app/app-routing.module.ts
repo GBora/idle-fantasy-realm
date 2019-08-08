80f3f169ab77100476ba0f4b1fcf82697a0faaf8
George Bora <gpbora@gmail.com>
2019-08-08 23:16:50 +0300

    Added bootstrap and ngb and chance, added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OfficeComponent } from './office/office.component';
+import { HrComponent } from './hr/hr.component';
+import { SalesComponent } from './sales/sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/office', pathMatch: 'full' },
+  { path: 'office', component: OfficeComponent },
+  { path: 'hr', component: HrComponent },
+  { path: 'sales', component: SalesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OfficeComponent } from './office/office.component';
+import { HrComponent } from './hr/hr.component';
+import { SalesComponent } from './sales/sales.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OfficeComponent,
+    HrComponent,
+    SalesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
